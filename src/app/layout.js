6671fcaa5aa35783e7e5088c898cf92d59f4d08e
Joseph Aloko <joseph.a@scopicsoftware.com>
2025-02-25 17:55:19 +0000

    Resolving import error for theme.js file, @mui/material-nextjs module not found and setting up a base homepage with material UI components

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,13 @@ import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import theme from "@/theme";
 
+export const metadata = {
+  title:
+    "TrueCacao | Premium Ghanaian Cocoa Powder & Dark Chocolate – Pure & Authentic",
+  description:
+    "Discover the rich taste of premium Ghanaian cocoa with TrueCacao. We offer 100% pure, high-quality raw cacao powder and dark chocolate, sourced from Ghana’s finest cocoa beans. Perfect for baking, beverages, and healthy indulgence. Shop now for the true cacao experience!",
+};
+
 export default function RootLayout(props) {
   return (
     <html lang="en">
